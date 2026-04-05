1547edbd6bca9bcbc9085b782e0aef76699c52c3
Sky388-bit <skydefender388@gmail.com>
2026-04-05 15:19:06 -0400

    Update orders.py

--- api/models/orders.py
+++ api/models/orders.py
@@ -8,6 +8,7 @@ class Order(Base):
     __tablename__ = "orders"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    customer_id = Column(Integer, ForeignKey("customer.id"))
     customer_name = Column(String(100))
     tracking_number = Column(String(100), nullable=False)
     order_status = Column(String(100), nullable=False)
@@ -15,4 +16,5 @@ class Order(Base):
     description = Column(String(300))
     total_price = Column(DECIMAL(10,2))
 
+    customer = relationship("Customer", back_populates="orders")
     order_details = relationship("OrderDetail", back_populates="order")
