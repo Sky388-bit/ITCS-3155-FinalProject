7ee8f7e445764b0c330233280ca75ddfc0bd728a
Alex Reine <alexreine712@gmail.com>
2026-04-28 11:22:15 -0400

    fix: correct field mismatch in order_details controller from sandwich_id to menu_id

--- api/controllers/order_details.py
+++ api/controllers/order_details.py
@@ -7,7 +7,7 @@ from sqlalchemy.exc import SQLAlchemyError
 def create(db: Session, request):
     new_item = model.OrderDetail(
         order_id=request.order_id,
-        sandwich_id=request.sandwich_id,
+        menu_id=request.menu_id,
         amount=request.amount
     )
 
